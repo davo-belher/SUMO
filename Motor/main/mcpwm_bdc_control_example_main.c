fe3726185646dcefd7aeea6160a4fa23c4dc4756
davo-belher <davo.belher@comunidad.unam.mx>
2024-03-28 23:04:17 -0600

    fixed commit

--- Motor/main/mcpwm_bdc_control_example_main.c
+++ Motor/main/mcpwm_bdc_control_example_main.c
@@ -15,7 +15,7 @@
 #include "bdc_motor.h"
 #include "pid_ctrl.h"
 
-static const char *TAG = "example";
+static const char *TAG = "STEP";
 
 // Enable this config,  we will print debug formated string, which in return can be captured and parsed by Serial-Studio
 #define SERIAL_STUDIO_DEBUG           CONFIG_SERIAL_STUDIO_DEBUG
@@ -23,8 +23,10 @@ static const char *TAG = "example";
 #define BDC_MCPWM_TIMER_RESOLUTION_HZ 10000000 // 10MHz, 1 tick = 0.1us
 #define BDC_MCPWM_FREQ_HZ             25000    // 25KHz PWM
 #define BDC_MCPWM_DUTY_TICK_MAX       (BDC_MCPWM_TIMER_RESOLUTION_HZ / BDC_MCPWM_FREQ_HZ) // maximum value we can set for the duty cycle, in ticks
-#define BDC_MCPWM_GPIO_A              7
-#define BDC_MCPWM_GPIO_B              15
+#define BDC_MCPWM_GPIO_A              21
+#define BDC_MCPWM_GPIO_B              22
+#define BDC_MCPWM_GPIO_C              19
+#define BDC_MCPWM_GPIO_D              18
 
 // #define BDC_ENCODER_GPIO_A            36
 // #define BDC_ENCODER_GPIO_B            35
@@ -71,18 +73,30 @@ void app_main(void)
     //     .pcnt_encoder = NULL,
     // };
 
-    ESP_LOGI(TAG, "Create DC motor");
-    bdc_motor_config_t motor_config = {
+    ESP_LOGI(TAG, "Create DC motor 1");
+    bdc_motor_config_t motor_1_config = {
         .pwm_freq_hz = BDC_MCPWM_FREQ_HZ,
         .pwma_gpio_num = BDC_MCPWM_GPIO_A,
         .pwmb_gpio_num = BDC_MCPWM_GPIO_B,
     };
-    bdc_motor_mcpwm_config_t mcpwm_config = {
+    ESP_LOGI(TAG, "Create DC motor 2");
+    bdc_motor_config_t motor_2_config = {
+        .pwm_freq_hz = BDC_MCPWM_FREQ_HZ,
+        .pwma_gpio_num = BDC_MCPWM_GPIO_C,
+        .pwmb_gpio_num = BDC_MCPWM_GPIO_D,
+    };
+    bdc_motor_mcpwm_config_t mcpwm_1_config = {
         .group_id = 0,
         .resolution_hz = BDC_MCPWM_TIMER_RESOLUTION_HZ,
     };
-    bdc_motor_handle_t motor = NULL;
-    ESP_ERROR_CHECK(bdc_motor_new_mcpwm_device(&motor_config, &mcpwm_config, &motor));
+    bdc_motor_mcpwm_config_t mcpwm_2_config = {
+        .group_id = 1,
+        .resolution_hz = BDC_MCPWM_TIMER_RESOLUTION_HZ,
+    };
+    bdc_motor_handle_t motor_1 = NULL;
+    ESP_ERROR_CHECK(bdc_motor_new_mcpwm_device(&motor_1_config, &mcpwm_1_config, &motor_1));
+    bdc_motor_handle_t motor_2 = NULL;
+    ESP_ERROR_CHECK(bdc_motor_new_mcpwm_device(&motor_2_config, &mcpwm_2_config, &motor_2));
     // motor_ctrl_ctx.motor = motor;
 
     // ESP_LOGI(TAG, "Init pcnt driver to decode rotary signal");
@@ -147,8 +161,9 @@ void app_main(void)
     // esp_timer_handle_t pid_loop_timer = NULL;
     // ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &pid_loop_timer));
 
-    ESP_LOGI(TAG, "Enable motor");
-    ESP_ERROR_CHECK(bdc_motor_enable(motor));
+    ESP_LOGI(TAG, "Enable motors");
+    ESP_ERROR_CHECK(bdc_motor_enable(motor_1));
+    ESP_ERROR_CHECK(bdc_motor_enable(motor_2));
     // ESP_LOGI(TAG, "Forward motor");
     // ESP_ERROR_CHECK(bdc_motor_forward(motor));
 
@@ -156,20 +171,29 @@ void app_main(void)
     // ESP_ERROR_CHECK(esp_timer_start_periodic(pid_loop_timer, BDC_PID_LOOP_PERIOD_MS * 1000));
 
     while (1) {
-        // vTaskDelay(pdMS_TO_TICKS(100));
+//         vTaskDelay(pdMS_TO_TICKS(100));
 //         // the following logging format is according to the requirement of serial-studio frame format
 //         // also see the dashboard config file `serial-studio-dashboard.json` for more information
 // #if SERIAL_STUDIO_DEBUG
 //         printf("/*%d*/\r\n", motor_ctrl_ctx.report_pulses);
 // #endif
-        ESP_LOGI(TAG, "Forward motor");
-        ESP_ERROR_CHECK(bdc_motor_forward(motor));
+        ESP_LOGI(TAG, "Forward motor 1");
+        ESP_ERROR_CHECK(bdc_motor_forward(motor_1));
         vTaskDelay(pdMS_TO_TICKS(2000));
-        ESP_LOGI(TAG, "reverse motor");
-        ESP_ERROR_CHECK(bdc_motor_reverse(motor));
+        ESP_LOGI(TAG, "reverse motor 1");
+        ESP_ERROR_CHECK(bdc_motor_reverse(motor_1));
         vTaskDelay(pdMS_TO_TICKS(2000));
-        ESP_LOGI(TAG, "stop motor");
-        ESP_ERROR_CHECK(bdc_motor_coast(motor));
+        ESP_LOGI(TAG, "stop motor 1");
+        ESP_ERROR_CHECK(bdc_motor_coast(motor_1));
+        vTaskDelay(pdMS_TO_TICKS(2000));   
+        ESP_LOGI(TAG, "Forward motor 2");
+        ESP_ERROR_CHECK(bdc_motor_forward(motor_2));
+        vTaskDelay(pdMS_TO_TICKS(2000));
+        ESP_LOGI(TAG, "reverse motor 2");
+        ESP_ERROR_CHECK(bdc_motor_reverse(motor_2));
+        vTaskDelay(pdMS_TO_TICKS(2000));
+        ESP_LOGI(TAG, "stop motor 2");
+        ESP_ERROR_CHECK(bdc_motor_coast(motor_2));
         vTaskDelay(pdMS_TO_TICKS(2000));   
     }
 }
